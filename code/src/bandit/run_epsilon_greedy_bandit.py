bcdf4e1db41f6c3f275734eecb6d05ed0db329f8
Greg Attra <greg.attra@gmail.com>
2022-01-29 16:45:35 -0500

    epsilon greedy run wroking

--- code/src/bandit/run_epsilon_greedy_bandit.py
+++ code/src/bandit/run_epsilon_greedy_bandit.py
@@ -4,13 +4,14 @@
 # 01/28/2022
 
 import argparse
+from lib.bandit.factory import EpsilonGreedyBanditFactory
 from lib.bandit.factory import BanditFactory, DomainFactory
 from lib.bandit.analytics import Analytics
 from lib.bandit.testsuite import TestSuite
 
 
 def main():
-    parser = argparse.ArgumentParser('Run a bandit testsuite')
+    parser = argparse.ArgumentParser('Run an epsilon greedy bandit testsuite')
     parser.add_argument('-p', help='The number of problems to run',
                         type=int, default=10)
     parser.add_argument('-s', help='The number of steps to run per problem',
@@ -21,13 +22,15 @@ def main():
                         type=float, default=.0)
     parser.add_argument('-std', help='The std of q*',
                         type=float, default=1.)
+    parser.add_argument('-e', help='Epsilon value for the bandit',
+                        type=float, default=.1)
     args = parser.parse_args()
 
     analytics = Analytics(args.p, args.s, args.k)
     testsuite = TestSuite(
         args.p,
         args.s,
-        BanditFactory(args.k),
+        EpsilonGreedyBanditFactory(args.k, args.e),
         DomainFactory(args.mu, args.std, args.k),
         analytics)
     testsuite.run()
