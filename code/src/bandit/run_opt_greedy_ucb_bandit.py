ee559efa50dddbaa8860896a856ec6e3c8437f94
Greg Attra <greg.attra@gmail.com>
2022-01-30 12:44:19 -0500

    implemented opt eps and ucb

--- code/src/bandit/run_opt_greedy_ucb_bandit.py
+++ code/src/bandit/run_opt_greedy_ucb_bandit.py
@@ -6,8 +6,10 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-
-from lib.bandit.analytics import EpsilonGreedyBanditAnalytics
+from lib.bandit.factory import OptimisticGreedyBanditFactory
+from lib.bandit.analytics import OptimisticGreedyBanditAnalytics
+from lib.bandit.analytics import UcbBanditAnalytics
+from lib.bandit.factory import UcbBanditFactory
 from lib.bandit.factory import EpsilonGreedyBanditFactory
 from lib.bandit.factory import DomainFactory
 from lib.bandit.testsuite import TestSuite
@@ -26,22 +28,37 @@ def main():
     parser.add_argument('-std', help='The std of q*',
                         type=float, default=1.)
     parser.add_argument('-eps', help='Epsilon values for each bandit',
-                        nargs='+', type=float, default=[.1])
+                        nargs='+', type=float, default=[.0, .0, .1, .1])
+    parser.add_argument('-c', help='The c param for the UCB action selectin algo',
+                        type=float, default=2.)
+    parser.add_argument('-q', help='The initial q values for optimistic greedy',
+                        nargs='+', type=float, default=[.0, 5., .0, 5.])
     args = parser.parse_args()
 
     analytics = []
     fig, ax = plt.subplots(2)
-    for eps in args.eps:
+    for q_init, eps in zip(args.q, args.eps):
         np.random.seed(0)
-        analytics = EpsilonGreedyBanditAnalytics(eps, args.p, args.s, args.k)
+        analytics = OptimisticGreedyBanditAnalytics(q_init, eps, args.p, args.s, args.k)
         testsuite = TestSuite(
             args.p,
             args.s,
-            EpsilonGreedyBanditFactory(args.k, eps),
+            OptimisticGreedyBanditFactory(q_init, eps, args.k),
             DomainFactory(args.mu, args.std, args.k),
             analytics)
         testsuite.run()
         analytics.splot(ax)
+
+    np.random.seed(0)
+    analytics = UcbBanditAnalytics(args.c, args.p, args.s, args.k)
+    testsuite = TestSuite(
+        args.p,
+        args.s,
+        UcbBanditFactory(args.c, args.k),
+        DomainFactory(args.mu, args.std, args.k),
+        analytics)
+    testsuite.run()
+    analytics.splot(ax)
 
     plt.show()
 
